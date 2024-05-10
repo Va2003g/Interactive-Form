9405e8b34c4cfa8b31b3c58c62dac1ab57b9fa47
vansh.gupta3 <vansh.gupta@geekyants.com>
2024-05-10 14:38:58 +0530

    Solved show and hide password problem

--- index.js
+++ index.js
@@ -6,111 +6,110 @@ console.log(errObject);
 // errObject[5].style.display = "block";
 //validations.
 
-function nameHandler(event)
-{
+function nameHandler(event) {
     const name = event.target.value;
     const regEx = /^[A-Za-z]+\ [A-Za-z]+$/;
-    if(name!='' && regEx.test(name))
-    {
+    const regEx2 = /^[A-Za-z]+$/;
+    if (name != '' && (regEx.test(name) || regEx2.test(name))) {
         console.log('Name ok');
-        localStorage.setItem("Name",name);
+        localStorage.setItem("Name", name);
         errObject[0].style.display = "none";
-    }else{
+    } else {
         errObject[0].style.display = "block";
     }
 }
 
-function emailHandler(event)
-{
+function emailHandler(event) {
     const email = event.target.value;
-    const regEx = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-    if(regEx.test(email))
-    {
+    const regEx = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z.]{2,}$/;
+    if (regEx.test(email)) {
         console.log('email ok');
-        localStorage.setItem("Email Id",email);
+        localStorage.setItem("Email Id", email);
         errObject[1].style.display = "none";
-    }else{
+    } else {
         errObject[1].style.display = "block";
     }
 }
 
-function pswdHandler(event)
-{
+function pswdHandler(event) {
     const pswd = event.target.value;
-    if(pswd.length >=8 )
-    {
+    if (pswd.length >= 8 || pswd.length == 0) {
         console.log('password ok');
-        localStorage.setItem("Password",pswd);
+        localStorage.setItem("Password", pswd);
         errObject[2].style.display = "none";
     }
-    else
-    {
-        errObject[2].style.display = 'block'
+    else {
+        errObject[2].style.display = 'block';
+        document.getElementsByClassName('showPswd')[0].style.top = "46%";
+        document.getElementsByClassName('showcnfPswd')[0].style.top = "46%";
     }
 }
-function cnfpswdHandler(event)
-{
+function cnfpswdHandler(event) {
     const cnfpswd = event.target.value;
     const pswd = document.getElementById('password').value;
-    if(pswd === cnfpswd )
-    {
+    if (pswd === cnfpswd) {
         console.log('confirm password ok');
-        localStorage.setItem("Confirm Password",cnfpswd);
+        localStorage.setItem("Confirm Password", cnfpswd);
         errObject[3].style.display = "none";
     }
-    else
-    {
-        errObject[3].style.display = 'block'
+    else {
+        errObject[3].style.display = 'block';
+        document.getElementsByClassName('showcnfPswd')[0].style.top = "46%";
+        document.getElementsByClassName('showPswd')[0].style.top = "46%";
     }
 }
 
-function phoneHandler(event)
-{
+function phoneHandler(event) {
     const phoneno = event.target.value;
-    if(phoneno.length==10)
-    {
-        localStorage.setItem('Phone No ',phoneno);
+    if (phoneno.length == 10) {
+        localStorage.setItem('Phone No ', phoneno);
         errObject[4].style.display = 'none';
     }
-    else
-    {
+    else {
         errObject[4].style.display = 'block';
     }
 }
 
-function submitHandler()
-{
+function submitHandler(event) {
+    event.preventDefault();
     console.log(localStorage);
 }
 
-document.getElementById('dob').addEventListener('change',(event)=>{
+document.getElementById('dob').addEventListener('change', (event) => {
     const date = event.target.value;
-    localStorage.setItem('Date Of Birth',date.split('-').reverse().join('-'));
+    localStorage.setItem('Date Of Birth', date.split('-').reverse().join('-'));
 })
-document.getElementById('gender').addEventListener('change',(event)=>{
+document.getElementById('gender').addEventListener('change', (event) => {
     const data = event.target.value;
-    localStorage.setItem('Gender',data);
+    localStorage.setItem('Gender', data);
 })
 
 
-//Show and Hide Passwords
-document.querySelector('.showPswd').addEventListener('click',()=>{
-    document.querySelector('.showPswd').style.visibility = 'hidden';
-    document.querySelector('.hidePswd').style.visibility = 'visible';
-    document.querySelector('#password').type = 'text';
-})
-document.querySelector('.hidePswd').addEventListener('click',()=>{
-    document.querySelector('.showPswd').style.visibility = 'visible';
-    document.querySelector('.hidePswd').style.visibility = 'hidden';
-    document.querySelector('#password').type = 'password';
-})
-document.querySelector('.showcnfPswd').addEventListener('click',()=>{
-    document.querySelector('.showcnfPswd').style.visibility = 'hidden';
-    document.querySelector('.hidecnfPswd').style.visibility = 'visible';
-    document.querySelector('#cnfPswd').type = 'text';
-})
-document.querySelector('.hidecnfPswd').addEventListener('click',()=>{
-    document.querySelector('.showcnfPswd').style.visibility = 'visible';
-    document.querySelector('.hidecnfPswd').style.visibility = 'hidden';
-    document.querySelector('#cnfPswd').type = 'password';
-})
+let showPswd = document.querySelector('.showPswd');
+console.log(showPswd);
+showPswd.addEventListener('click', () => {
+    const passwordInput = document.querySelector('#password');
+    const imgNode = document.querySelector('.showPswd');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        imgNode.src = './images/blind.png';
+    } else {
+        passwordInput.type = 'password';
+        imgNode.src = './images/view.png';
+    }
+});
+
+let showcnfPswd = document.querySelector('.showcnfPswd');
+showcnfPswd.addEventListener('click', () => {
+    const passwordInput = document.querySelector('#cnfPswd');
+    const imgNode = document.querySelector('.showcnfPswd');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        imgNode.src = './images/blind.png';
+    } else {
+        passwordInput.type = 'password';
+        imgNode.src = './images/view.png';
+    }
+});
